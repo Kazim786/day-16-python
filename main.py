62c6bcd73491035915b5bebc6a9d4468d777cf61
Kazim Shabbir <Kazimshabbir2010@hotmail.com>
2021-01-04 14:40:07 -0600

    2 tables. 1 using row method and the other using column adding method

--- main.py
+++ main.py
@@ -1,25 +1,44 @@
 
-# import another_module
+# # import another_module
 
-# print(another_module.another_var)
+# # print(another_module.another_var)
 
-# import turtle 
+# # import turtle 
 
-# timmy = turtle.Turtle() #To make syntax more like the powerpoints you can import like this
+# # timmy = turtle.Turtle() #To make syntax more like the powerpoints you can import like this
 
-from turtle import Turtle, Screen
+# from turtle import Turtle, Screen
 
-timmy = Turtle() 
-# print(timmy)
+# timmy = Turtle() 
+# # print(timmy)
 
-timmy.shape("turtle") #Another object function for the turtle class
+# timmy.shape("turtle") #Another object function for the turtle class
 
-timmy.color("green")
+# timmy.color("green")
 
-timmy.forward(100)
+# timmy.forward(100)
 
-my_screen = Screen()
+# my_screen = Screen()
 
-print(my_screen.canvheight) #object attribute
+# print(my_screen.canvheight) #object attribute
 
-my_screen.exitonclick() #object method
+# my_screen.exitonclick() #object method
+
+from prettytable import PrettyTable
+
+table1 = PrettyTable()
+print(table1)
+
+table1.field_names = ["First name", "Last name", "SSN"]
+
+table1.add_row(["Kazim", "Shabbir", 334])
+print(table1)
+
+
+table = PrettyTable()
+table.add_column("City Name", ["Houston", "Chicago", "Seattle", "Boston", "NYC"])
+print(table)
+
+table.add_column("Population", ["12000", "13000", "1472", "78634", "66666"])
+
+print(table)
