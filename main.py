7dc620945576977c2b959098a7ebda5a689055ef
Kazim Shabbir <Kazimshabbir2010@hotmail.com>
2021-01-04 13:40:47 -0600

    classes

---
+++ main.py
@@ -0,0 +1,12 @@
+
+# import another_module
+
+# print(another_module.another_var)
+
+# import turtle 
+
+# timmy = turtle.Turtle() #To make syntax more like the powerpoints you can import like this
+
+from turtle import Turtle
+
+timmy = Turtle()
