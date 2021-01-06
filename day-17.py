b83a4b76543e7675c49c4a96b5794fcb422dbf39
Kazim Shabbir <Kazimshabbir2010@hotmail.com>
2021-01-06 13:47:40 -0600

    fixed attributes

--- day-17.py
+++ day-17.py
@@ -11,7 +11,7 @@ class User:
 
 
 #Now you have to pass the username and id info into the parameters of the User() class. This will set the values for the objects
-user_1 = User("529590", "Kazim")
+user_1 = User("Kazim", "529590")
 
 #To add attribute you just use dot notation. This is bad practice though
 # user_1.id = "529590"
