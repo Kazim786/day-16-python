4b288053295e934077ef1c262b63838937a668dd
Kazim Shabbir <Kazimshabbir2010@hotmail.com>
2021-01-06 13:46:05 -0600

    attibutes

--- day-17.py
+++ day-17.py
@@ -1,10 +1,20 @@
 #Creating your own classes
 
-class User: 
-    pass
 
-user_1 = User()
 
-#To add attribute you just use dot notation
-user_1.id = "529590"
-user_1.username = "Kazim"
+
+
+class User:
+    def __init__(self, username, id):
+        self.username = username
+        self.id = id
+
+
+#Now you have to pass the username and id info into the parameters of the User() class. This will set the values for the objects
+user_1 = User("529590", "Kazim")
+
+#To add attribute you just use dot notation. This is bad practice though
+# user_1.id = "529590"
+# user_1.username = "Kazim"
+
+print(user_1.id)
